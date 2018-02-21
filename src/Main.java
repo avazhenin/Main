e9ae345d2be6ca57b4a1d2128031f0e58ae540e9
Anatoliy <Anatoliy@DESKTOP-091S6GA>
2018-02-22 01:47:41 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Anatoliy
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
+		
+	}
+
+}
